98fa7fa0b83b9ada53c3e91fd9419e1bf33b767d
michalszuba <szuba.m.michal@gamil.com>
2022-11-19 18:22:29 +0100

    termCarRentalBegin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from 'react-hook-form'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const { register, handleSubmit } = useForm()
+
+	const handleOnSubmit = data => {
+		console.log(data)
+		// let rentalPrices = basePrice * data[termC]
+	}
+
+	const fuelPrice = 5.5
+	const basePrice = 1000
+	const location = ''
+	const averageFuel = 8
+	const availableModels = 2
+	const priceCategoryCar = {
+		Basic: 1,
+		Standard: 1.3,
+		Medium: 1.6,
+		Premium: 2,
+	}
+
+	return (
+		<form onSubmit={handleSubmit(handleOnSubmit)}>
+			<label>
+				Szacunkowa ilość kilometrów do przejechania
+				<br />
+				<input {...register('distance')} type='number' />
+			</label>
+			<br />
+			<label>
+				Rok otrzymania prawa jazdy
+				<br />
+				<input {...register('yearDrivingLicense')} type='number' />
+			</label>
+			<br />
+			<label>
+				Termin wypożyczenia samochodu
+				<br />
+				Od:
+				<input {...register('termCarRentalBegin')} type='date' />
+				<br />
+				do:
+				<input {...register('termCarRentalEnd')} type='date' />
+			</label>
+			<br />
+			<input
+				type='submit'
+				value='submit'
+				onClick={() => {
+					console.log('fuelPrice: ' + fuelPrice + '\npriceCategoryCar: ' + priceCategoryCar)
+				}}
+			/>
+		</form>
+	)
 }
-
-export default App;
